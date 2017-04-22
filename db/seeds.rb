2524263008b6497ff277685cdf3e48164521fe3a
scott789 <turnscott100@gmail.com>
2017-04-21 20:58:08 -0400

    Updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+List.create(name: "Grocery")
+
+Item.create(name: "soda", list_id: 2, position: 1)
+Item.create(name: "eggs", list_id: 2, position: 2)
+Item.create(name: "cheese", list_id: 2, position: 3)
