274bcba6d81e0227828d48087e198384aad69189
scott789 <turnscott100@gmail.com>
2017-04-21 21:04:35 -0400

    drop user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170421225530) do
+ActiveRecord::Schema.define(version: 20170422010338) do
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
@@ -29,20 +29,5 @@ ActiveRecord::Schema.define(version: 20170421225530) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "username"
-    t.string   "password"
-    t.string   "email"
-    t.integer  "group_id"
-    t.integer  "role_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "users", ["group_id"], name: "index_users_on_group_id"
-  add_index "users", ["role_id"], name: "index_users_on_role_id"
 
 end
